24672e484f670442ba3466c71d32f6f27224a522
dima <dimavasylyna@gmail.com>
2019-07-31 13:10:37 +0300

    доробив перевірку на пусті поля + додавання класу невалідним елементам

--- validate.js
+++ validate.js
@@ -1,5 +1,5 @@
 window.isValide = (obj) => {
-
+// console.log(obj);
 	
 	let hasEmpty = (arr)=> {
 		if (arr) {
@@ -19,15 +19,7 @@ window.isValide = (obj) => {
 		}
 		return true;
 	}
-	
-	let checkObjQuestion = (obj) => {
-		for (let q in obj) {
-			if (obj[q].question.length === 0) {
-				return false;
-			} 
-		}
-		return true;
-	}
+
 	let checkObjRightAnswer = (obj) => {
 		for (let q in obj) {
 			if (obj[q].answer && !obj[q].rightAnswer) {
@@ -37,22 +29,72 @@ window.isValide = (obj) => {
 		return true;
 	}
 
+	let checkObjQuestion = (obj) => {
+		let numberErrorList = [];
+		for (let q in obj) {
+			// якщо є пусте поле, то додаємо його номер в масив numberErrorList
+			if (obj[q].question.length === 0) {	
+				numberErrorList.push(q.match(/\d+/)[0]);
+			} 
+		}
+		if (numberErrorList.length > 0) {
+			return {
+				numberErrorList: numberErrorList,
+				result: false
+			}
+		} else {
+			return {
+				result: true
+			}
+		}
+	}
 	
-	
+let checkObjQuestionRes = checkObjQuestion(obj);
+
+
+// ф-ція, яка дає класи невалідним елементам
+// elementClass - клас елементів, які будуть перевірятися і їм же додаватиметься клас error
+// numberErrorListOfQuestionBlock - масив номерів невалідних питань
+let addClassError = (elementClass, numberErrorListOfQuestionBlock) => {
+	let questionBlockList = document.querySelectorAll(elementClass);
+	questionBlockList.forEach((questionBlock)=>{
+		questionBlock.classList.remove(`error`);
+	});
+	numberErrorListOfQuestionBlock.forEach((num)=>{
+		questionBlockList[num - 1].classList.add(`error`);
+	});
+}
+
+
+
+
+
 	
 	if (Object.keys(obj).length === 0 && obj.constructor === Object) {
 		alert(`Не створено жодного запитання!`);
 		return false;
-	} else if (!checkObjQuestion(obj)) {
-		alert(`Не всі поля заповнені!`);
+	} else if (!checkObjQuestionRes.result) {
+		addClassError(`.creator__item`, checkObjQuestionRes.numberErrorList);
+		// console.log(document.querySelectorAll(`.creator__item`)[0]);
 		return false;
-	} else if (!checkObjAnswer(obj)) {
-		alert(`Не всі відповіді заповнені!`);
-		return false;
-	} else if (!checkObjRightAnswer(obj)) {
-		alert(`Не вказано вірних відповідей для автопідрахунку`);
-		return false;
-	}  else {
-	 	return true;
-	 }
+	} else {
+		return true;
+	}
+	
+	// if (Object.keys(obj).length === 0 && obj.constructor === Object) {
+	// 	alert(`Не створено жодного запитання!`);
+	// 	return false;
+	// } else if (!checkObjQuestion(obj).result) {
+	// 	console.log(checkObjQuestion(obj).emptyElementNumber);
+	// 	alert(`Не всі поля заповнені!`);
+	// 	return false;
+	// } else if (!checkObjAnswer(obj)) {
+	// 	alert(`Не всі відповіді заповнені!`);
+	// 	return false;
+	// } else if (!checkObjRightAnswer(obj)) {
+	// 	alert(`Не вказано вірних відповідей для автопідрахунку`);
+	// 	return false;
+	// }  else {
+	//  	return true;
+	//  }
 }
